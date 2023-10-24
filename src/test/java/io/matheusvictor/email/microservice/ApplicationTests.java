c3790505d9305f09d7dd011a9b67a45bddf3755c
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-24 20:38:16 -0300

    feat: inital commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/test/java/io/matheusvictor/email/microservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matheusvictor.email.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
